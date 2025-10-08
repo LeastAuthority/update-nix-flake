6c267588d03f210222898431dc13e6290c7713ca
Benoit Donneaux <benoit@leastauthority.com>
2025-10-08 11:48:58 +0200

    Add CI check and a dir option to test with a fixture flake
    Signed-off-by: Benoit Donneaux <benoit@leastauthority.com>

---
+++ tests/fixtures/basic-flake/flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs = {
+    # The nixpkgs channels we want to consume
+    nixpkgs-25_05.url = "github:NixOS/nixpkgs/nixos-25.05";
+
+    # Some links to the above channels for consistent naming in outputs
+    nixpkgs.follows = "nixpkgs-25_05";
+  };
+  outputs = { self, nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells."${system}".default = pkgs.mkShell {
+        packages = with pkgs; [
+          bash
+        ];
+      };
+    };
+}
